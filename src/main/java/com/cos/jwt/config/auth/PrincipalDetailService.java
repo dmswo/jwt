488349e196f697deea0d1fd936246ca1a667448e
dmswo106 <dmswo106@naver.com>
2021-12-20 22:28:55 +0900

    jwt를 이용한 로그인 구현

---
+++ src/main/java/com/cos/jwt/config/auth/PrincipalDetailService.java
@@ -0,0 +1,24 @@
+package com.cos.jwt.config.auth;
+
+import com.cos.jwt.model.User;
+import com.cos.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// http://localhost:8080/login => formLogin을 사용안하기 때문에 동작을 안함
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsername(username);
+        System.out.println("userEntity : "+userEntity);
+        return new PrincipalDetails(userEntity);
+    }
+}
